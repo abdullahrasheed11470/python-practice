f938ff0a912e07da7c2933fcba5b4d7b59b45b76
Muhammad Abdullah <abdullahrasheed11470@gmail.com>
2025-05-19 17:27:02 +0500

    add assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,28 @@
+print("table of 1")
+for i in range(1,11):
+    print(f"{i} ", end='')
+print("\n")
+print('table of 2')
+
+for i in range(1,21):
+    if i % 2 == 0:
+        print(f"{i} ", end='')
+print("\n")
+print('table of 3')
+
+for i in range(1,31):
+    if i % 3 == 0:
+        print(f"{i} ", end='')
+print("\n")
+print('table of 4')
+
+for i in range(1,41):
+    if i % 4 == 0:
+        print(f"{i} ", end='')
+print("\n")
+print('table of 5')
+
+for i in range(1,51):
+    if i % 5 == 0:
+        print(f"{i} ", end='')
+print("\n")
